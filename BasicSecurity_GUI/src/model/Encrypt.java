dc39e1b6a1c2d0618c2262385c29b26e0514b0cf
Chris Bruno <Bruno.Chris.1992@gmail.com>
2015-05-16 16:47:53 +0200

    In comments staat waar wat moet gebeuren

--- BasicSecurity_GUI/src/model/Encrypt.java
+++ BasicSecurity_GUI/src/model/Encrypt.java
@@ -17,176 +17,179 @@ import java.util.Scanner;
 import javax.crypto.Cipher;
 
 public class Encrypt {
-/**
-   * String to hold name of the encryption algorithm.
-   */
-  public static final String ALGORITHM = "RSA";
+	/**
+	 * String to hold name of the encryption algorithm.
+	 */
+	public static final String ALGORITHM = "RSA";
 
-  /**
-   * String to hold the name of the private key file.
-   */
-  public static final String PRIVATE_KEY_FILE = "C:/keys/private.key";
+	/**
+	 * String to hold the name of the private key file.
+	 */
+	public static final String PRIVATE_KEY_FILE = "C:/keys/private.key";
 
-  /**
-   * String to hold name of the public key file.
-   */
-  public static final String PUBLIC_KEY_FILE = "C:/keys/public.key";
+	/**
+	 * String to hold name of the public key file.
+	 */
+	public static final String PUBLIC_KEY_FILE = "C:/keys/public.key";
 
-  /**
-   * Generate key which contains a pair of private and public key using 1024
-   * bytes. Store the set of keys in Prvate.key and Public.key files.
-   * 
-   * @throws NoSuchAlgorithmException
-   * @throws IOException
-   * @throws FileNotFoundException
-   */
-  public static void generateKey() {
-    try {
-      final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
-      keyGen.initialize(1024);
-      final KeyPair key = keyGen.generateKeyPair();
+	/**
+	 * Generate key which contains a pair of private and public key using 1024
+	 * bytes. Store the set of keys in Prvate.key and Public.key files.
+	 * 
+	 * @throws NoSuchAlgorithmException
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	public static void generateKey(String name) {
+		try {
+			final KeyPairGenerator keyGen = KeyPairGenerator
+					.getInstance(ALGORITHM);
+			keyGen.initialize(1024);
+			final KeyPair key = keyGen.generateKeyPair();
 
-      File privateKeyFile = new File(PRIVATE_KEY_FILE);
-      File publicKeyFile = new File(PUBLIC_KEY_FILE);
+			File privateKeyFile = new File(name + "_privateKey.key");
+			File publicKeyFile = new File(name + "_publicKey.key");
 
-      // Create files to store public and private key
-      if (privateKeyFile.getParentFile() != null) {
-        privateKeyFile.getParentFile().mkdirs();
-      }
-      privateKeyFile.createNewFile();
+			// Create files to store public and private key
+			if (privateKeyFile.getParentFile() != null) {
+				privateKeyFile.getParentFile().mkdirs();
+			}
+			privateKeyFile.createNewFile();
 
-      if (publicKeyFile.getParentFile() != null) {
-        publicKeyFile.getParentFile().mkdirs();
-      }
-      publicKeyFile.createNewFile();
+			if (publicKeyFile.getParentFile() != null) {
+				publicKeyFile.getParentFile().mkdirs();
+			}
+			publicKeyFile.createNewFile();
 
-      // Saving the Public key in a file
-      ObjectOutputStream publicKeyOS = new ObjectOutputStream(
-          new FileOutputStream(publicKeyFile));
-      publicKeyOS.writeObject(key.getPublic());
-      publicKeyOS.close();
+			// Saving the Public key in a file
+			ObjectOutputStream publicKeyOS = new ObjectOutputStream(
+					new FileOutputStream(publicKeyFile));
+			publicKeyOS.writeObject(key.getPublic());
+			publicKeyOS.close();
 
-      // Saving the Private key in a file
-      ObjectOutputStream privateKeyOS = new ObjectOutputStream(
-          new FileOutputStream(privateKeyFile));
-      privateKeyOS.writeObject(key.getPrivate());
-      privateKeyOS.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+			// Saving the Private key in a file
+			ObjectOutputStream privateKeyOS = new ObjectOutputStream(
+					new FileOutputStream(privateKeyFile));
+			privateKeyOS.writeObject(key.getPrivate());
+			privateKeyOS.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-  }
+	}
 
-  /**
-   * The method checks if the pair of public and private key has been generated.
-   * 
-   * @return flag indicating if the pair of keys were generated.
-   */
-  public static boolean areKeysPresent() {
+	/**
+	 * The method checks if the pair of public and private key has been
+	 * generated.
+	 * 
+	 * @return flag indicating if the pair of keys were generated.
+	 */
+	public static boolean areKeysPresent() {
 
-    File privateKey = new File(PRIVATE_KEY_FILE);
-    File publicKey = new File(PUBLIC_KEY_FILE);
+		File privateKey = new File(PRIVATE_KEY_FILE);
+		File publicKey = new File(PUBLIC_KEY_FILE);
 
-    if (privateKey.exists() && publicKey.exists()) {
-      return true;
-    }
-    return false;
-  }
+		if (privateKey.exists() && publicKey.exists()) {
+			return true;
+		}
+		return false;
+	}
 
-  /**
-   * Encrypt the plain text using public key.
-   * 
-   * @param text
-   *          : original plain text
-   * @param key
-   *          :The public key
-   * @return Encrypted text
-   * @throws java.lang.Exception
-   */
-  public static byte[] encrypt(String text, PublicKey key) {
-    byte[] cipherText = null;
-    try {
-      // get an RSA cipher object and print the provider
-      final Cipher cipher = Cipher.getInstance(ALGORITHM);
-      // encrypt the plain text using the public key
-      cipher.init(Cipher.ENCRYPT_MODE, key);
-      cipherText = cipher.doFinal(text.getBytes());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return cipherText;
-  }
+	/**
+	 * Encrypt the plain text using public key.
+	 * 
+	 * @param text
+	 *            : original plain text
+	 * @param key
+	 *            :The public key
+	 * @return Encrypted text
+	 * @throws java.lang.Exception
+	 */
+	public static byte[] encrypt(String text, PublicKey key) {
+		byte[] cipherText = null;
+		try {
+			// get an RSA cipher object and print the provider
+			final Cipher cipher = Cipher.getInstance(ALGORITHM);
+			// encrypt the plain text using the public key
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			cipherText = cipher.doFinal(text.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cipherText;
+	}
 
-  /**
-   * Decrypt text using private key.
-   * 
-   * @param text
-   *          :encrypted text
-   * @param key
-   *          :The private key
-   * @return plain text
-   * @throws java.lang.Exception
-   */
-  public static String decrypt(byte[] text, PrivateKey key) {
-    byte[] dectyptedText = null;
-    try {
-      // get an RSA cipher object and print the provider
-      final Cipher cipher = Cipher.getInstance(ALGORITHM);
+	/**
+	 * Decrypt text using private key.
+	 * 
+	 * @param text
+	 *            :encrypted text
+	 * @param key
+	 *            :The private key
+	 * @return plain text
+	 * @throws java.lang.Exception
+	 */
+	public static String decrypt(byte[] text, PrivateKey key) {
+		byte[] dectyptedText = null;
+		try {
+			// get an RSA cipher object and print the provider
+			final Cipher cipher = Cipher.getInstance(ALGORITHM);
 
-      // decrypt the text using the private key
-      cipher.init(Cipher.DECRYPT_MODE, key);
-      dectyptedText = cipher.doFinal(text);
+			// decrypt the text using the private key
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			dectyptedText = cipher.doFinal(text);
 
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-    return new String(dectyptedText);
-  }
+		return new String(dectyptedText);
+	}
 
-  /**
-   * Test the EncryptionUtil
-   */
-  public static void main(String[] args) {
+	/**
+	 * Test the EncryptionUtil
+	 */
+	public static void main(String[] args) {
 
-    try {
+		try {
 
-      // Check if the pair of keys are present else generate those.
-      if (!areKeysPresent()) {
-        // Method generates a pair of keys using the RSA algorithm and stores it
-        // in their respective files
-        generateKey();
-      }
+			// Check if the pair of keys are present else generate those.
+			/*
+			 * if (!areKeysPresent()) { // Method generates a pair of keys using
+			 * the RSA algorithm and stores it // in their respective files
+			 * generateKey(); }
+			 */
+			Scanner sc = new Scanner(new FileInputStream("Lol.txt"));
 
-      Scanner sc = new Scanner(new FileInputStream("Lol.txt"));
-      
-      String originalText = "";
-      String line = null;
-      while(sc.hasNext()){
-    	  line = sc.nextLine();
-    	  originalText += line + "\n";
-      }
-      
-   //   final String originalText = sc.nextLine();
-      ObjectInputStream inputStream = null;
+			String originalText = "";
+			String line = null;
+			while (sc.hasNext()) {
+				line = sc.nextLine();
+				originalText += line + "\n";
+			}
 
-      // Encrypt the string using the public key
-      inputStream = new ObjectInputStream(new FileInputStream(PUBLIC_KEY_FILE));
-      final PublicKey publicKey = (PublicKey) inputStream.readObject();
-      final byte[] cipherText = encrypt(originalText, publicKey);
+			// final String originalText = sc.nextLine();
+			ObjectInputStream inputStream = null;
 
-      // Decrypt the cipher text using the private key.
-      inputStream = new ObjectInputStream(new FileInputStream(PRIVATE_KEY_FILE));
-      final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
-      final String plainText = decrypt(cipherText, privateKey);
+			// Encrypt the string using the public key
+			inputStream = new ObjectInputStream(new FileInputStream(
+					PUBLIC_KEY_FILE));
+			final PublicKey publicKey = (PublicKey) inputStream.readObject();
+			final byte[] cipherText = encrypt(originalText, publicKey);
 
-      // Printing the Original, Encrypted and Decrypted Text
-      System.out.println("Original: " + originalText);
-      System.out.println("Encrypted: " +cipherText.toString());
-      System.out.println("Decrypted: " + plainText);
+			// Decrypt the cipher text using the private key.
+			inputStream = new ObjectInputStream(new FileInputStream(
+					PRIVATE_KEY_FILE));
+			final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
+			final String plainText = decrypt(cipherText, privateKey);
 
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+			// Printing the Original, Encrypted and Decrypted Text
+			System.out.println("Original: " + originalText);
+			System.out.println("Encrypted: " + cipherText.toString());
+			System.out.println("Decrypted: " + plainText);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- BasicSecurity_GUI/src/view/GUIController.java
+++ BasicSecurity_GUI/src/view/GUIController.java
@@ -4,14 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Scanner;
 
 import javax.swing.JFileChooser;
-
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -23,7 +24,7 @@ import javafx.scene.control.*;
 public class GUIController {
 
 	private MainApplication mainApp;
-	
+
 	@FXML
 	private Button submit;
 	@FXML
@@ -51,45 +52,52 @@ public class GUIController {
 	@FXML
 	private TextField textFile;
 	@FXML
+	private TextField nameField;
+	@FXML
+	private TextField nameFieldCor;
+	@FXML
+	private Button genBtn;
+	@FXML
 	private TextArea encFileArea;
-	
+
 	@FXML
 	private RadioButton encRb;
 	@FXML
 	private RadioButton decRb;
-	
+
 	private ToggleGroup rbGroup;
-	
+
 	private boolean encrypt;
-	
-	public GUIController(){
-		
+
+	public GUIController() {
+
 	}
-	
-	public void initialize(){
+
+	public void initialize() {
 		rbGroup = new ToggleGroup();
 		encRb.setToggleGroup(rbGroup);
 		decRb.setToggleGroup(rbGroup);
 		rbGroup.selectedToggleProperty().addListener(new OnToggleHandler());
-	//	SubmitHandler handler = new SubmitHandler();
-	//	submit.setOnAction(handler);
-		
+		SubmitHandler handler = new SubmitHandler();
+		submit.setOnAction(handler);
+
 		changeEnc.setOnAction(this::openFile);
 		changeFile2.setOnAction(this::openFile);
 		changeFile3.setOnAction(this::openFile);
 		changePriv.setOnAction(this::openFile);
 		changePub.setOnAction(this::openFile);
 		changeFile.setOnAction(this::openFile);
-		
+		genBtn.setOnAction(this::generate);
+
 		welcomeText();
 	}
-	
-	private void welcomeText(){
+
+	private void welcomeText() {
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(
 					"Tutorial.txt"));
 			String line;
-			while((line = in.readLine()) != null){
+			while ((line = in.readLine()) != null) {
 				encFileArea.appendText(line + "\n");
 			}
 		} catch (FileNotFoundException e) {
@@ -101,149 +109,238 @@ public class GUIController {
 		}
 	}
 
-	private class OnToggleHandler implements ChangeListener<Toggle>{
+	private class OnToggleHandler implements ChangeListener<Toggle> {
 
 		@Override
-		public void changed(ObservableValue<? extends Toggle> ov,
-				Toggle t, Toggle t1) {
+		public void changed(ObservableValue<? extends Toggle> ov, Toggle t,
+				Toggle t1) {
 			// TODO Auto-generated method stub
 			System.out.println("Observable : " + ov);
 			System.out.println("Old Value : " + t);
 			System.out.println("New Value : " + t1);
-			RadioButton rb = (RadioButton)t1.getToggleGroup().getSelectedToggle();
-			switch(rb.getId()){
-			case "encRb" : encrypt = true;
+			RadioButton rb = (RadioButton) t1.getToggleGroup()
+					.getSelectedToggle();
+			switch (rb.getId()) {
+			case "encRb":
+				encrypt = true;
 				break;
-			case "decRb" : encrypt = false;
+			case "decRb":
+				encrypt = false;
 				break;
 			}
-			
-			if(encrypt){
+
+			if (encrypt) {
 				disableDecrypt();
 				enableEncrypt();
-			}else
-			{
+			} else {
 				disableEncrypt();
 				enableDecrypt();
 			}
-			
-		}
-		
-	}	
 
-	public void disableEncrypt(){
+		}
+
+	}
+
+	public void generate(ActionEvent Event) {
+		String n1 = nameField.getText();
+		String n2 = nameFieldCor.getText();
+		model.Encrypt.generateKey(n1);
+		model.Encrypt.generateKey(n2);
+	}
+
+	public void disableEncrypt() {
 		changeFile.setDisable(true);
 		textFile.setDisable(true);
 	}
-	
-	public void disableDecrypt(){
+
+	public void disableDecrypt() {
 		changeEnc.setDisable(true);
 		changeFile2.setDisable(true);
 		changeFile3.setDisable(true);
 		changePriv.setDisable(true);
 		changePub.setDisable(true);
-		
+
 		textEnc.setDisable(true);
 		textFile2.setDisable(true);
 		textFile3.setDisable(true);
 		textPriv.setDisable(true);
 		textPub.setDisable(true);
 	}
-	
-	public void enableEncrypt(){
+
+	public void enableEncrypt() {
 		changeFile.setDisable(false);
 		textFile.setDisable(false);
 	}
-	
-	public void enableDecrypt(){
+
+	public void enableDecrypt() {
 		changeEnc.setDisable(false);
 		changeFile2.setDisable(false);
 		changeFile3.setDisable(false);
 		changePriv.setDisable(false);
 		changePub.setDisable(false);
-		
+
 		textEnc.setDisable(false);
 		textFile2.setDisable(false);
 		textFile3.setDisable(false);
 		textPriv.setDisable(false);
 		textPub.setDisable(false);
 	}
-	
-	private void openFile(ActionEvent event){
-		Button name = (Button)event.getSource();
+
+	private void openFile(ActionEvent event) {
+		Button name = (Button) event.getSource();
 		System.out.println(name);
 		JFileChooser chooser = new JFileChooser();
 		int returnValue = chooser.showOpenDialog(null);
 		if (returnValue == JFileChooser.APPROVE_OPTION) {
 			File selectedFile = chooser.getSelectedFile();
-			switch(name.getId()){
-			case "changeEnc": textEnc.setText(selectedFile.getName().toString());
+			switch (name.getId()) {
+			case "changeEnc":
+				textEnc.setText(selectedFile.getName().toString());
 				break;
-			case "changeFile2": textFile2.setText(selectedFile.getName().toString());
+			case "changeFile2":
+				textFile2.setText(selectedFile.getName().toString());
 				break;
-			case "changeFile3": textFile3.setText(selectedFile.getName().toString());
+			case "changeFile3":
+				textFile3.setText(selectedFile.getName().toString());
 				break;
-			case "changePriv": textPriv.setText(selectedFile.getName().toString());
+			case "changePriv":
+				textPriv.setText(selectedFile.getName().toString());
 				break;
-			case "changePub" : textPub.setText(selectedFile.getName().toString());
+			case "changePub":
+				textPub.setText(selectedFile.getName().toString());
 				break;
-			case "changeFile" : textFile.setText(selectedFile.getName().toString());
+			case "changeFile":
+				textFile.setText(selectedFile.getName().toString());
 				break;
-			default: System.out.println("Something went wrong!");
+			default:
+				System.out.println("Something went wrong!");
 			}
 			/*
-			 * encFileArea.clear();
-			 * try {
-				BufferedReader in = new BufferedReader(new FileReader(
-						selectedFile));
-				String line = null;
-				while ((line = in.readLine()) != null) {
-					encFileArea.appendText(line + "\n");
-				}
-			} catch (FileNotFoundException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}*/
+			 * encFileArea.clear(); try { BufferedReader in = new
+			 * BufferedReader(new FileReader( selectedFile)); String line =
+			 * null; while ((line = in.readLine()) != null) {
+			 * encFileArea.appendText(line + "\n"); } } catch
+			 * (FileNotFoundException e2) { // TODO Auto-generated catch block
+			 * e2.printStackTrace(); } catch (IOException e1) { // TODO
+			 * Auto-generated catch block e1.printStackTrace(); }
+			 */
 
 		}
-		class SubmitHandler implements EventHandler<ActionEvent>{
 
-			@Override
-			public void handle(ActionEvent event) {
-		
-		try {
+	}
 
-		      // Check if the pair of keys are present else generate those.
-		      if (!model.Encrypt.areKeysPresent()) {
-		        // Method generates a pair of keys using the RSA algorithm and stores it
-		        // in their respective files
-		       model.Encrypt.generateKey();
-		      }
+	class SubmitHandler implements EventHandler<ActionEvent> {
 
-		      final String originalText = "Text to be encrypted ";
-		      ObjectInputStream inputStream = null;
+		@Override
+		public void handle(ActionEvent event) {
 
-		      // Encrypt the string using the public key
-		      inputStream = new ObjectInputStream(new FileInputStream(model.Encrypt.PUBLIC_KEY_FILE));
-		      final PublicKey publicKey = (PublicKey) inputStream.readObject();
-		      final byte[] cipherText = model.Encrypt.encrypt(originalText, publicKey);
+			ObjectInputStream inputStream;
+			byte[] cipherText = null;
+			String text = null;
+			String plainText = null;
+			try {
 
-		      // Decrypt the cipher text using the private key.
-		      inputStream = new ObjectInputStream(new FileInputStream(model.Encrypt.PRIVATE_KEY_FILE));
-		      final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
-		      final String plainText = model.Encrypt.decrypt(cipherText, privateKey);
+				if (encrypt) {
 
-		      // Printing the Original, Encrypted and Decrypted Text
-		      System.out.println("Original: " + originalText);
-		      System.out.println("Encrypted: " +cipherText.toString());
-		      System.out.println("Decrypted: " + plainText);
+					/*
+					 * DES Encryption van file!
+					 */
 
-		    } catch (Exception e) {
-		      e.printStackTrace();
-		    }
+					/*
+					 * RSA encryptie van de DES Key met de public key van
+					 * ontvanger
+					 */
+					Scanner sc = new Scanner(new FileInputStream("Key.txt"));
+					text = sc.next();
+					inputStream = null;
+
+					// Encrypt the string using the public key
+					inputStream = new ObjectInputStream(new FileInputStream(
+							"public.key"));
+					final PublicKey publicKey = (PublicKey) inputStream
+							.readObject();
+					cipherText = model.Encrypt.encrypt(text, publicKey);
+					
+					FileOutputStream out = new FileOutputStream("encDes.txt"); // wegschrijven geencrypteerde des sleutel
+
+					/*
+					 * Hash van file (oorspronkelijke boodschap)
+					 */
+
+					/*
+					 * Encryptie hash met private key van de gebruiker
+					 */
+
+				}
+
+				if (!encrypt) {
+					/*
+					 * 
+					 * Decryptie van de DES Key
+					 * 
+					 * Hier moet de geencrypteerde sleutel worden uitgelezen en worden gedecrypteerd
+					 */
+
+					inputStream = new ObjectInputStream(new FileInputStream(
+					/* model.Encrypt.PRIVATE_KEY_FILE */"private.key"));
+					final PrivateKey privateKey = (PrivateKey) inputStream
+							.readObject();
+					plainText = model.Encrypt.decrypt(cipherText,
+							privateKey);
+
+					/*
+					 * 
+					 * Decryptie boodschap met DES Key
+					 */
+
+					/*
+					 * 
+					 * Berekenen Hash van boodschap
+					 */
+
+					/*
+					 * Decryptie hash file(file 3) met public key gebruiker
+					 */
+
+					/*
+					 * Vergelijken verkregen hash met zelf berekende hash
+					 */
+
+				}
+
+				/*
+				 * // final String originalText = "Text to be encrypted ";
+				 * Scanner sc = new Scanner(new FileInputStream("Key.txt"));
+				 * String text = sc.next(); ObjectInputStream inputStream =
+				 * null;
+				 * 
+				 * // Encrypt the string using the public key inputStream = new
+				 * ObjectInputStream(new
+				 * FileInputStream(model.Encrypt.PUBLIC_KEY_FILE)); final
+				 * PublicKey publicKey = (PublicKey) inputStream.readObject();
+				 * final byte[] cipherText = model.Encrypt.encrypt(text,
+				 * publicKey);
+				 */
+
+			/*	// Decrypt the cipher text using the private key.
+				inputStream = new ObjectInputStream(new FileInputStream(
+																		  model.
+																		  Encrypt
+																		  .
+																		  PRIVATE_KEY_FILE
+																		 ""));
+				final PrivateKey privateKey = (PrivateKey) inputStream
+						.readObject();
+				final String plainText = model.Encrypt.decrypt(cipherText,
+						privateKey);
+			 */
+				// Printing the Original, Encrypted and Decrypted Text
+				System.out.println("Original: " + text);
+				System.out.println("Encrypted: " + cipherText.toString());
+				System.out.println("Decrypted: " + plainText);
+
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
